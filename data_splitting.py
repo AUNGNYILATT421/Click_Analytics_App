61390e23a22710275d7da65fdbc713c1e0223d33
Ezio123-Kenway <eziokenway054@gmail.com>
2025-12-30 17:25:39 +0700

    updated data_splitting.py

--- data_splitting.py
+++ data_splitting.py
@@ -39,7 +39,7 @@ X_train, X_test, y_train, y_test = train_test_split(df.drop('{target}', axis=1),
             with col1:
                 st.write("Train")
                 st.write(f"X Train Shape: {X_train.shape}")
-                st.write(f"Y Train Shape: {y_train.shape}")
+                st.write(f"y Train Shape: {y_train.shape}")
                 train = pd.concat([X_train, y_train], axis=1)
                 train_csv = train.to_csv(index=False).encode('utf-8')
                 st.download_button("Download Train Set", train_csv, "train.csv", "text/csv", key='train2')
